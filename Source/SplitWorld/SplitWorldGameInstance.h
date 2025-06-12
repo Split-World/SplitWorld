f3616c999adee6505df7f99aa9d99d27e97bf6be
양승권 <ysk102706@gmail.com>
2025-06-12 17:46:26 +0900

    온라인 서브시스템 추가

---
+++ Source/SplitWorld/SplitWorldGameInstance.h
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "SplitWorldGameInstance.generated.h"
+
+UCLASS()
+class SPLITWORLD_API USplitWorldGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+};
